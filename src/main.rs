5fec80ea4c1f372c3e2bc9024e38a83059109d60
Tony LaForgia <alforgia428@gmail.com>
2023-01-10 00:49:42 -0600

    if w/ let statement

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,26 @@
-// else/if
+// if in a let statement
 fn main() {
-    let number = 6;
-    if number % 4 == 0 {
-        println!("number is divisible by 4");
-    } else if number % 3 == 0 {
-        println!("number is divisible by 3");
-    } else if number % 2 == 0 {
-        println!("number is divisible by 2");
-    } else {
-        println!("number is not divisible by 4, 3, or 2");
-    }
+    let condition = true;
+    let number = if condition { 5 } else { 6 };
+    println!("The value of number is: {}", number);
 }
 
+// else/if
+// fn main() {
+// Rust will find the first true block and not look for the rest.
+//     let number = 6;
+//     if number % 4 == 0 {
+//         println!("number is divisible by 4");
+//     } else if number % 3 == 0 {
+//         println!("number is divisible by 3");
+//     } else if number % 2 == 0 {
+//         println!("number is divisible by 2");
+//     } else {
+//         println!("number is not divisible by 4, 3, or 2");
+//     }
+// }
+//
+//
 // if/else conditions
 //
 // fn main() {
